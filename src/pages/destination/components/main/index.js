7f2061e398c78918489807f767c39baa443f761e
lvvorz <lvvorz@foxmail.com>
2021-09-30 00:28:45 +0800

    completed tab-content

--- src/pages/destination/components/main/index.js
+++ src/pages/destination/components/main/index.js
@@ -1,5 +1,27 @@
 import './main.css';
-import '../tab';
-import '../content';
+import Tab from '../tab';
+import Content from '../content';
 import 'components/loading';
 
+const tabEl = document.querySelector('.tab');
+const tab = new Tab(tabEl);
+const content = new Content(document.getElementById('destination-content'));
+
+const itemEls = tabEl.querySelectorAll('.tab-item');
+
+tabEl.addEventListener('click', e => {
+    const itemEl = e.target;
+
+    if(itemEl.classList.contains('tab-item')) {
+        const index = itemEl.dataset.id - 1;
+        const tabPromise = tab.to(index);
+        
+        content.setLoading();
+
+        tabPromise.then(data => {
+            content.set(data);
+        });
+    }
+}, false);
+
+itemEls[0].click();
--- src/pages/destination/components/tab/index.js
+++ src/pages/destination/components/tab/index.js
@@ -1,1 +1,24 @@
 import './tab.css';
+import {getData, getDelayedData} from 'api/getData';
+import {URL, TAB_ITEM_CLASS, TAB_ACTIVE_ITEM_CLASS_NAME} from './config.js';
+class Tab{
+    constructor(el) {
+        this.itemList = el.querySelectorAll(TAB_ITEM_CLASS);
+    }
+
+    setActiveItem(index) {
+        for(const item of this.itemList) {
+            item.classList.remove(TAB_ACTIVE_ITEM_CLASS_NAME);
+        }
+
+        this.itemList[index].classList.add(TAB_ACTIVE_ITEM_CLASS_NAME);
+    }
+
+    to(index) {
+        this.setActiveItem(index);
+        return getData(`${URL}/${this.itemList[index].dataset.id}`);
+    }
+    
+}
+
+export default Tab;
